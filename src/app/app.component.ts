c0f67f6c58f71f73afe3fefe31adbc02bbc0ac7a
rakshitabalikai <rakshitabalikai09@gmail.com>
2025-04-15 20:11:05 +0530

    food recipies angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavComponent,HomeComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <app-nav></app-nav>
+    <app-home></app-home>
+    
+   
+    <p>hello there</p>
+    <p>{{var1}}</p>
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
   title = 'FoodRecipes';
+  var1=10;
 }
